b2795e646a31b186e572d52daa16404138c1eb49
ameralahbabi <58179860+ameralahbabi@users.noreply.github.com>
2019-11-25 20:04:30 +0400

    Add files via upload

---
+++ Capstone Project - Module 1.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Capstone project is performed on this notebook
+
+# In[1]:
+
+
+import pandas as pd
+import numpy as np
+
+
+# In[2]:
+
+
+print('Hello Capstone project course!')
+
+
+# In[ ]:
+
+
+
+
